b863d84e40a5d2fab5fb28b9a7bff2f9f66d6df7
Brian Terlson <brian.terlson@microsoft.com>
2017-09-25 15:28:14 -0700

    Updates for production

--- src/server/ghstrategy.ts
+++ src/server/ghstrategy.ts
@@ -1,12 +1,16 @@
 import { Strategy as GitHubStrategy } from 'passport-github';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from './secrets';
 import GHAuthUser from '../shared/GitHubAuthenticatedUser';
+const callbackURL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://tcq.azurewebsites.net/auth/github/callback'
+    : 'http://127.0.0.1:3000http://tcq.azurewebsites.net/auth/github/callback';
 
 export default new GitHubStrategy(
   {
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://127.0.0.1:3000/auth/github/callback',
+    callbackURL,
     scope: ['user:email', 'read:org']
   },
   function(accessToken, refreshToken, profile, cb) {

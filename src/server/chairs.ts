b196354c70605a06eefd1b09f3b721039a2b57fd
Brian Terlson <brian.terlson@microsoft.com>
2017-09-26 11:03:29 -0700

    add littledan, leo, and rex as chairs

--- src/server/chairs.ts
+++ src/server/chairs.ts
@@ -1,4 +1,10 @@
-let chairs = new Set(['11236' /* brian */, '972891' /* daniel */]);
+let chairs = new Set([
+  '11236' /* brian */,
+  '972891' /* daniel */,
+  '189835' /* littledan */,
+  '301201' /* leo balter */,
+  '6708936' /* rex */
+]);
 export function isChair(userid: string) {
   return chairs.has(userid);
 }
